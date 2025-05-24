3ffc313fd845c70b1ef6f7e80c95c5ccd2752ef4
unknown <demonkos130@gmail.com>
2025-05-23 22:21:37 -0400

    pl creation n status message

--- index.js
+++ index.js
@@ -5,3 +5,19 @@ const playerForm = document.getElementById('playerForm');
 let currentPlayer = 'X';
 let gameActive = true;
 let gameState = ['', '', '', '', '', '', '', '', ''];
+
+const winningConditions = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+];
+
+const statusMessage = document.createElement('p');
+statusMessage.textContent = `Current Player: ${currentPlayer}`;
+
+document.body.appendChild(statusMessage);
