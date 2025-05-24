d9bf1a48dd4c036d8bd5c847bd9f0ce0c880c8cf
unknown <demonkos130@gmail.com>
2025-05-23 22:17:09 -0400

    edited the script setting game state

---
+++ index.js
@@ -0,0 +1,7 @@
+const resetButton = document.getElementById('resetBtn');
+const gameBoard = document.getElementById('box');
+const playerForm = document.getElementById('playerForm');
+
+let currentPlayer = 'X';
+let gameActive = true;
+let gameState = ['', '', '', '', '', '', '', '', ''];
